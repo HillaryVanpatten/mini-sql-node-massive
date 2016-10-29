254ea9fe60d247838ee40af283fb46664145e225
HillaryVanpatten <hillaryvanpatten@gmail.com>
2016-10-29 10:41:28 -0600

    Finished

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var massive = require('massive');
+var connectionString = "postgres://hillaryvanpatten@localhost/sandbox";
 
+
+var massiveInstance = massive.connectSync({connectionString : connectionString})
 var app = module.exports = express();
+
+var controller = require('./controller.js');
+app.set('db', massiveInstance);
+var db = app.get('db');
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
+  console.log("Successfully listening on : 3000")
 })
 
+controller.getPlanes();
+// db.new_plane(function(err, planes){
+    // console.log(err, "plane added")
+// });
+
+// db.get_planes(function(err, planes){
+//     console.log(err, planes)
+// })
